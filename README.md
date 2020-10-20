467219ee8822da68ced74ec759553696d2aa6d9e
damminhtien <damminhtienchl@gmail.com>
2020-10-20 19:12:37 +0700

    release

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Happy woman day 2019
-
-https://happy-woman-day-2019.herokuapp.com/
-
-See old version:
-
-https://happywomanday2018.herokuapp.com/
-
-https://happy-woman-day-2017.herokuapp.com/
-# happy-woman-day-vn-2020
 # happy-woman-day-vn-2020
---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+let countHeartOver = 0;
+let audio = new Audio("../music/beat.mp3");
+
+for (i = 0; i < 800; i++)
+  document.body.innerHTML += '<div id="' + i + '" class="TwitterHeart"></div>';
+
+window.addEventListener("mouseover", HeartAnimation);
+
+function HeartAnimation(event) {
+  if (event.target.tagName != "HTML") {
+    try {
+      audio.play();
+      countHeartOver++;
+      var targetHeart = document.getElementById(event.target.id);
+      var heartStateCounter = 1;
+      var timer = setInterval(function () {
+        if (heartStateCounter != 29 && targetHeart)
+          targetHeart.style.backgroundPosition =
+            -100 * heartStateCounter++ + "px 0";
+        else {
+          clearInterval(timer);
+        }
+      }, 100);
+    } catch (err) {
+      // console.log(err)
+    }
+
+    let randomNumber = Math.floor(Math.random() * 10);
+    document.getElementById("image" + randomNumber).style.opacity = 1;
+    setTimeout(function () {
+      document.getElementById("image" + randomNumber).style.opacity = 0;
+    }, 5000);
+  }
+}
